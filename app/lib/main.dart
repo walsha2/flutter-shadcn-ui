c2242fbbc903b1fef094d9b5b4ae8dc6f3e36ce7
Wahab Alshahin <wahab.alshahin@gmail.com>
2024-09-07 17:53:14 -0400

    Reproduce form error

---
+++ app/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ShadForm(
+        child: Text('Form Content'),
+      ),
+    );
+  }
+}
